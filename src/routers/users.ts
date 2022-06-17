bb644c442a5d91e2dc6822e7d024ce9ac362b865
Catalin <catalin.nicolaescu@deepcode.xyz>
2022-06-18 01:34:33 +0300

    fixed authentication

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -6,7 +6,11 @@ import type { User } from '../repositories/types';
 
 import { BadRequestError, UnauthorizedError } from '../errors';
 import { hashPassword, generateToken } from '../security';
-import { initTokenValidationRequestHandler, initAdminValidationRequestHandler, RequestAuth } from '../middleware/security';
+import {
+  initTokenValidationRequestHandler,
+  initAdminValidationRequestHandler,
+  RequestAuth,
+} from '../middleware/security';
 import { UserType } from '../constants';
 
 export function initUsersRouter(sequelizeClient: SequelizeClient): Router {
@@ -15,70 +19,92 @@ export function initUsersRouter(sequelizeClient: SequelizeClient): Router {
   const tokenValidation = initTokenValidationRequestHandler(sequelizeClient);
   const adminValidation = initAdminValidationRequestHandler();
 
-  router.route('/')
+  router
+    .route('/')
     .get(tokenValidation, initListUsersRequestHandler(sequelizeClient))
-    .post(tokenValidation, adminValidation, initCreateUserRequestHandler(sequelizeClient));
+    .post(
+      tokenValidation,
+      adminValidation,
+      initCreateUserRequestHandler(sequelizeClient)
+    );
 
-  router.route('/login')
-    .post(tokenValidation, initLoginUserRequestHandler(sequelizeClient));
-  router.route('/register')
+  router.route('/login').post(initLoginUserRequestHandler(sequelizeClient));
+  router
+    .route('/register')
     .post(initRegisterUserRequestHandler(sequelizeClient));
 
   return router;
 }
 
-function initListUsersRequestHandler(sequelizeClient: SequelizeClient): RequestHandler {
+function initListUsersRequestHandler(
+  sequelizeClient: SequelizeClient
+): RequestHandler {
   return async function listUsersRequestHandler(req, res, next): Promise<void> {
     const { models } = sequelizeClient;
 
     try {
-      const { auth: { user: { type: userType } } } = req as unknown as { auth: RequestAuth };
+      const {
+        auth: {
+          user: { type: userType },
+        },
+      } = req as unknown as { auth: RequestAuth };
 
       const isAdmin = userType === UserType.ADMIN;
 
       const users = await models.users.findAll({
         attributes: isAdmin ? ['id', 'name', 'email'] : ['name', 'email'],
-        ...!isAdmin && { where: { type: { [Op.ne]: UserType.ADMIN } } },
+        ...(!isAdmin && { where: { type: { [Op.ne]: UserType.ADMIN } } }),
         raw: true,
       });
 
       res.send(users);
 
-      return res.end();
+      res.end();
     } catch (error) {
       next(error);
     }
   };
 }
 
-function initCreateUserRequestHandler(sequelizeClient: SequelizeClient): RequestHandler {
-  return async function createUserRequestHandler(req, res, next): Promise<void> {
+function initCreateUserRequestHandler(
+  sequelizeClient: SequelizeClient
+): RequestHandler {
+  return async function createUserRequestHandler(
+    req,
+    res,
+    next
+  ): Promise<void> {
     try {
       // NOTE(roman): missing validation and cleaning
       const { type, name, email, password } = req.body as CreateUserData;
 
       await createUser({ type, name, email, password }, sequelizeClient);
 
-      return res.status(204).end();
+      res.status(204).end();
     } catch (error) {
       next(error);
     }
   };
 }
 
-function initLoginUserRequestHandler(sequelizeClient: SequelizeClient): RequestHandler {
+function initLoginUserRequestHandler(
+  sequelizeClient: SequelizeClient
+): RequestHandler {
   return async function loginUserRequestHandler(req, res, next): Promise<void> {
     const { models } = sequelizeClient;
 
     try {
       // NOTE(roman): missing validation and cleaning
-      const { email, password } = req.body as { name: string; email: string; password: string };
+      const { email, password } = req.body as {
+        email: string;
+        password: string;
+      };
 
-      const user = await models.users.findOne({
+      const user = (await models.users.findOne({
         attributes: ['id', 'passwordHash'],
         where: { email },
         raw: true,
-      }) as Pick<User, 'id' | 'passwordHash'> | null;
+      })) as Pick<User, 'id' | 'passwordHash'> | null;
       if (!user) {
         throw new UnauthorizedError('EMAIL_OR_PASSWORD_INCORRECT');
       }
@@ -89,43 +115,59 @@ function initLoginUserRequestHandler(sequelizeClient: SequelizeClient): RequestH
 
       const token = generateToken({ id: user.id });
 
-      return res.send({ token }).end();
+      res.send({ token }).end();
     } catch (error) {
       next(error);
     }
   };
 }
 
-function initRegisterUserRequestHandler(sequelizeClient: SequelizeClient): RequestHandler {
-  return async function createUserRequestHandler(req, res, next): Promise<void> {
+function initRegisterUserRequestHandler(
+  sequelizeClient: SequelizeClient
+): RequestHandler {
+  return async function createUserRequestHandler(
+    req,
+    res,
+    next
+  ): Promise<void> {
     try {
       // NOTE(roman): missing validation and cleaning
-      const { name, email, password } = req.body as Omit<CreateUserData, 'type'>;
+      const { name, email, password } = req.body as Omit<
+        CreateUserData,
+        'type'
+      >;
 
-      await createUser({ type: UserType.BLOGGER, name, email, password }, sequelizeClient);
-
-      return res.status(204).end();
+      await createUser(
+        {
+          type: UserType.BLOGGER,
+          name,
+          email,
+          password,
+        },
+        sequelizeClient
+      );
+      res.status(204).end();
     } catch (error) {
       next(error);
     }
   };
 }
 
-async function createUser(data: CreateUserData, sequelizeClient: SequelizeClient): Promise<void> {
+async function createUser(
+  data: CreateUserData,
+  sequelizeClient: SequelizeClient
+): Promise<void> {
   const { type, name, email, password } = data;
 
   const { models } = sequelizeClient;
 
-  const similarUser = await models.users.findOne({
+  const similarUser = (await models.users.findOne({
     attributes: ['id', 'name', 'email'],
     where: {
-      [Op.or]: [
-        { name },
-        { email },
-      ],
+      [Op.or]: [{ name }, { email }],
     },
     raw: true,
-  }) as Pick<User, 'id' | 'name' | 'email'> | null;
+  })) as Pick<User, 'id' | 'name' | 'email'> | null;
   if (similarUser) {
     if (similarUser.name === name) {
       throw new BadRequestError('NAME_ALREADY_USED');
@@ -135,7 +177,14 @@ async function createUser(data: CreateUserData, sequelizeClient: SequelizeClient
     }
   }
 
-  await models.users.create({ type, name, email, passwordHash: password });
+  await models.users.create({
+    type,
+    name,
+    email,
+    passwordHash: hashPassword(password),
+  });
 }
 
-type CreateUserData = Pick<User, 'type' | 'name' | 'email'> & { password: User['passwordHash'] };
+type CreateUserData = Pick<User, 'type' | 'name' | 'email'> & {
+  password: User['passwordHash'];
+};
