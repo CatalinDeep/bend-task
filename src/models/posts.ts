0a8bdf27ab3db950430c11d7acd19a7329cec708
Catalin <catalin.nicolaescu@deepcode.xyz>
2022-06-17 22:49:41 +0300

    bug-fix: set post id as primaryKey

--- src/models/posts.ts
+++ src/models/posts.ts
@@ -3,47 +3,52 @@ import { Sequelize, DataTypes } from 'sequelize';
 import { Post } from '../repositories/posts';
 
 export function setupPostsModel(modelName: string, sequelize: Sequelize): void {
-  Post.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
+  Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true,
+      },
+      isHidden: {
+        type: DataTypes.BOOLEAN,
+        field: 'is_hidden',
+        allowNull: false,
+        defaultValue: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        field: 'created_at',
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: 'updated_at',
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      unique: true,
-    },
-    isHidden: {
-      type: DataTypes.BOOLEAN,
-      field: 'is_hidden',
-      allowNull: false,
-      defaultValue: false,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      field: 'created_at',
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      field: 'updated_at',
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-  }, {
-    sequelize,
-    tableName: 'posts',
-    modelName,
-    name: {
-      singular: 'post',
-      plural: 'posts',
-    },
-    timestamps: true,
-  });
+    {
+      sequelize,
+      tableName: 'posts',
+      modelName,
+      name: {
+        singular: 'post',
+        plural: 'posts',
+      },
+      timestamps: true,
+    }
+  );
 }
